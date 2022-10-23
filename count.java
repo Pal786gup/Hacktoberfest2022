9554ebdae875d2810ca319ebe1908e09256d7d7f
Pal786gup <116503632+Pal786gup@users.noreply.github.com>
2022-10-23 21:06:04 +0530

    Create count.java

---
+++ count.java
@@ -0,0 +1,13 @@
+import java.util.*;
+  
+class Main {
+ 
+    public static void main(String[] args)
+    {
+        List<Integer> list = Arrays.asList(0, 2, 4, 6, 8, 10, 12);
+        long total = list.stream().count();
+  
+  
+        System.out.println(total);
+    }
+}
